17af7da8624b4988e59d8789348625e8728e3223
Fred Oliveira <fred@helloform.com>
2013-08-17 22:22:20 -0700

    Ruby version updates

--- modules/people/manifests/fredoliveira/development.pp
+++ modules/people/manifests/fredoliveira/development.pp
@@ -16,13 +16,18 @@ class people::fredoliveira::development {
   # ------- ruby + phantomjs --------
 
   class { 'ruby::global':
-    version => '1.9.3-p385'
+    version => '2.0.0-p247'
   }
 
   $dashboard_ruby = "1.9.3-p385"
+  $dashboard_pro_ruby = "2.0.0-p247"
 
   ruby::local { "${people::fredoliveira::params::my_projects}/dashboard":
     version => $dashboard_ruby
+  }
+
+  ruby::local { "${people::fredoliveira::params::my_projects}/dashboardpro":
+    version => $dashboard_pro_ruby
   }
 
   phantomjs::local { "${people::fredoliveira::params::my_projects}/dashboard":
